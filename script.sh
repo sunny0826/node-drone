7c0e64c99169558cef554a98d5f455f8a4a0d84f
郭旭东 <guoxudong@keking.cn>
2019-05-17 18:42:37 +0800

    export

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-ossutil config -e oss-cn-shanghai-internal.aliyuncs.com -i ${ACCESS_KEY_ID} -k ${ACCESS_KEY_SECRET}
+# ossutil config -e oss-cn-shanghai-internal.aliyuncs.com -i ${ACCESS_KEY_ID} -k ${ACCESS_KEY_SECRET}
+
+/bin/sh
