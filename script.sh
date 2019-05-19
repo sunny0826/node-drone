e5c3f2924d90c7e581df4bfb2b6cd656d23d78e6
郭旭东 <guoxudong@keking.cn>
2019-05-19 12:01:24 +0800

    fix .drone.yml

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ossutil config -e oss-cn-shanghai-internal.aliyuncs.com -i ${ACCESS_KEY_ID} -k ${ACCESS_KEY_SECRET}
+ossutil config -e oss-cn-shanghai-internal.aliyuncs.com -i ${PLUGIN_ACCESS_KEY_ID} -k ${PLUGIN_ACCESS_KEY_SECRET}
